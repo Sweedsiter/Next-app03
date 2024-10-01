48f9243e519a45c91056b0db32cb4f3680b0e65a
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-10-01 09:01:06 +0000

    2

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import ProductsList from "@/components/ProductsList";
 export default function Home() {
-  return <div className="p-5">Home</div>;
+  return (
+    <>
+      <ProductsList />
+    </>
+  );
 }
---
+++ components/ProductsList.jsx
@@ -0,0 +1,7 @@
+export default function ProductsList() {
+  return (
+    <>
+      <h1 className="p-3">Products List</h1>
+    </>
+  );
+}
