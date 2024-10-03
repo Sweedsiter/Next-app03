40378f33dc57eb2cbb5cd942bbea3d25f0169000
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-10-03 02:44:14 +0000

    nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa6";
+
 export default function Navbar() {
+  const [search, setSearch] = useState();
   return (
     <>
-      <h1>Navbar</h1>
+      <nav className="bg-slate-900 text-white py-2 px-4 flex justify-between">
+        <Link href={"/"} className="text-xl font-bold">
+          Dx-emb
+        </Link>
+        <div className="flex justify-between flex-row ">
+          <input
+            type="text"
+            placeholder="search"
+            className="pl-3 rounded-lg text-gray-950"
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+          {search}
+          <div className="pl-5 text-3xl">
+            <FaBars />
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
