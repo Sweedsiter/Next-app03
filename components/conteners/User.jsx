63822f08d553c74935263e07942c7532974907e0
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-11-06 04:31:33 +0000

    06/11/2024 V2

--- components/conteners/User.jsx
+++ components/conteners/User.jsx
@@ -4,31 +4,32 @@ import { BsBasket3Fill } from "react-icons/bs";
 export default function User() {
   const Basket = 9;
   return (
-    <div>
-      <h1 className="duration-150 hover:bg-slate-100 hover:pl-2 py-1 text-xl font-bold">
-        User Status
-      </h1>
-      <div className="text-sm py-1">
-        <div className="duration-150 hover:bg-slate-100 hover:pl-2 py-1">
-          ชื่อ : exsample
-        </div>
-        <div className="duration-150 hover:bg-slate-100 hover:pl-2 py-1">
-          เมล์ : exsample@mail.com
-        </div>
-      </div>
-      <div className="flex flex-row items-center justify-between text-sm py-1 duration-150 hover:bg-slate-100 hover:px-2 ">
-        <div>ยอด : 300-.</div>
-        <Link
-          href="/api/topics"
-          className="relative p-2 duration-300 hover:bg-slate-900 hover:text-white hover:rounded-lg hover:mr-1"
-        >
-          <div className="absolute text-xs bg-red-600 text-white px-1 top-0 right-0 h-fit w-fit rounded-md">
-            {Basket && Basket}
+    <>
+      <Link href={"/api/topics"}>
+        <h1 className="duration-150 hover:bg-slate-100 hover:pl-2 py-1 text-xl font-bold">
+          User Status
+        </h1>
+        <div className="text-sm py-1">
+          <div className="duration-150 hover:bg-slate-100 hover:pl-2 py-1">
+            ชื่อ : exsample
           </div>
-          <BsBasket3Fill className="text-2xl " />
+          <div className="duration-150 hover:bg-slate-100 hover:pl-2 py-1">
+            เมล์ : exsample@mail.com
+          </div>
+        </div>
+        <Link
+          href={"/api/topics"}
+          className="flex flex-row items-center justify-between text-sm py-1 duration-150 hover:bg-slate-100 hover:px-2 "
+        >
+          <div>ยอด : 300-.</div>
+          <div className="relative p-2">
+            <div className="absolute text-xs bg-red-600 text-white px-1 top-0 right-0 h-fit w-fit rounded-md">
+              {Basket && Basket}
+            </div>
+            <BsBasket3Fill className="text-2xl " />
+          </div>
         </Link>
-      </div>
-
+      </Link>
       <div className="flex flex-row justify-between my-5">
         <Link
           href="/"
@@ -37,6 +38,6 @@ export default function User() {
           Log-out
         </Link>
       </div>
-    </div>
+    </>
   );
 }
