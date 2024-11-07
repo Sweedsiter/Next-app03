51c1d9116d94c736a3b7f918d185173b8f6d6b25
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-11-07 04:04:30 +0000

    07/11/2024 V1

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,12 +2,34 @@ import Link from "next/link";
 
 export default function dashboard() {
   return (
-    <div className="h-full w-full m-5 p-5">
-      <h1 className="py-3">dashboard</h1>
+    <div className=" w-full relative border p-3">
+      <h1 className="p-2">dashboard</h1>
 
-      <Link href="/" className=" py-2">
+      <Link href="/" className=" p-2 ">
         <span className="p-1 border"> กลับหน้าเเรก</span>
       </Link>
+
+      <div className=" flex flex-col desktop:flex-row w-full items-center">
+        <div className="desktop:p-3 p-2">
+          <iframe
+            title="T-Shirt"
+            allow="autoplay;fullscreen;"
+            width="400"
+            height="400"
+            src="https://sketchfab.com/models/a8e5fb89e3104f4fb61a222063afc8f7/embed"
+          ></iframe>
+        </div>
+
+        <div className="desktop:p-3 p-2">
+          <iframe
+            title="Polo"
+            allow="autoplay;fullscreen;"
+            width="400"
+            height="400"
+            src="https://sketchfab.com/models/8bcc849495c34a96882420f3fd98329a/embed"
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 }
