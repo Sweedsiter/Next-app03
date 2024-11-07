3390c4075f755bcaf4501ac8e4d01db9e235a509
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-11-07 04:51:59 +0000

    00

--- components/conteners/3DShirts.jsx
+++ components/conteners/3DShirts.jsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function DShirts() {
   return (
     <div className=" flex flex-col desktop:flex-row w-full items-center ">
-      <div className="border rounded-lg border-slate-300 m-1 flex flex-col w-200 h-200 p-1  bg-white desktop:drop-shadow duration-300 hover:p-0 ">
+      <div className="border rounded-lg border-slate-300 m-1 flex flex-col p-1  bg-white desktop:drop-shadow duration-300 hover:p-0 ">
         <iframe
           title="T-Shirt"
           allow="autoplay;fullscreen;"
@@ -9,9 +11,21 @@ export default function DShirts() {
           height="340"
           src="https://sketchfab.com/models/a8e5fb89e3104f4fb61a222063afc8f7/embed"
         ></iframe>
+        <div className="p-2 w-full flex flex-row justify-between items-cenetr">
+          <span>เสื้อคอกลม ตัดต่อไหล่หน้า</span>
+          <Link
+            className="p-1 border"
+            target="_blank"
+            href={
+              "https://sketchfab.com/models/a8e5fb89e3104f4fb61a222063afc8f7/embed"
+            }
+          >
+            กดดูจอใหญ่
+          </Link>
+        </div>
       </div>
 
-      <div className="border rounded-lg border-slate-300 m-1 flex flex-col w-200 h-200 p-1  bg-white desktop:drop-shadow duration-300 hover:p-0 ">
+      <div className="border rounded-lg border-slate-300 m-1 flex flex-col p-1  bg-white desktop:drop-shadow duration-300 hover:p-0 ">
         <iframe
           title="Polo"
           allow="autoplay;fullscreen;"
@@ -19,6 +33,18 @@ export default function DShirts() {
           height="340"
           src="https://sketchfab.com/models/8bcc849495c34a96882420f3fd98329a/embed"
         ></iframe>
+        <div className="p-2 w-full flex flex-row justify-between items-cenetr">
+          <span>เสื้อโปโล กระดุม3เม็ด</span>
+          <Link
+            className="p-1 border"
+            target="_blank"
+            href={
+              "https://sketchfab.com/models/a8e5fb89e3104f4fb61a222063afc8f7/embed"
+            }
+          >
+            กดดูจอใหญ่
+          </Link>
+        </div>
       </div>
     </div>
   );
