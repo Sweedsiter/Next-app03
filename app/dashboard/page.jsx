5af298e0c6ad88812c65e51d09cd8ee772d6acad
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-11-07 04:28:10 +0000

    00

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,10 +3,10 @@ import DShirts from "@/components/conteners/3DShirts";
 
 export default function dashboard() {
   return (
-    <div className=" w-full relative border p-3">
+    <div className=" w-full flex flex-col items-center p-3">
       <h1 className="p-2">dashboard</h1>
       <DShirts />
-      <Link href="/" className=" p-2 ">
+      <Link href="/" className=" p-5 ">
         <span className="p-1 border"> กลับหน้าเเรก</span>
       </Link>
     </div>
--- components/conteners/3DShirts.jsx
+++ components/conteners/3DShirts.jsx
@@ -1,12 +1,12 @@
 export default function DShirts() {
   return (
-    <div className=" flex flex-col desktop:flex-row w-full items-center">
+    <div className=" flex flex-col desktop:flex-row w-full items-center ">
       <div className="border rounded-lg border-slate-300 m-1 flex flex-col w-200 h-200 p-1  bg-white desktop:drop-shadow duration-300 hover:p-0 ">
         <iframe
           title="T-Shirt"
           allow="autoplay;fullscreen;"
-          width="360"
-          height="360"
+          width="340"
+          height="340"
           src="https://sketchfab.com/models/a8e5fb89e3104f4fb61a222063afc8f7/embed"
         ></iframe>
       </div>
@@ -15,8 +15,8 @@ export default function DShirts() {
         <iframe
           title="Polo"
           allow="autoplay;fullscreen;"
-          width="360"
-          height="360"
+          width="340"
+          height="340"
           src="https://sketchfab.com/models/8bcc849495c34a96882420f3fd98329a/embed"
         ></iframe>
       </div>
