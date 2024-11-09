82be0042ad1f044a41168d0bb3945f463b6c8a53
Peter <47840148+Sweedsiter@users.noreply.github.com>
2024-11-09 03:14:03 +0000

    09/11/2024 V1

--- components/conteners/3DShirts.jsx
+++ components/conteners/3DShirts.jsx
@@ -1,8 +1,12 @@
 import Link from "next/link";
+import { ModelViewer } from "@/components/conteners/3Dmodel";
 
 export default function DShirts() {
   return (
     <div className=" flex flex-col desktop:flex-row w-full items-center ">
+      {/* 3D Model  */}
+      <ModelViewer />
+      {/* 3D */}
       <div className="border rounded-lg border-slate-300 m-1 flex flex-col p-1  bg-white desktop:drop-shadow duration-300 hover:p-0 ">
         <iframe
           title="T-Shirt"
@@ -24,7 +28,6 @@ export default function DShirts() {
           </Link>
         </div>
       </div>
-
       <div className="border rounded-lg border-slate-300 m-1 flex flex-col p-1  bg-white desktop:drop-shadow duration-300 hover:p-0 ">
         <iframe
           title="T-Shirt"
@@ -46,7 +49,6 @@ export default function DShirts() {
           </Link>
         </div>
       </div>
-
       <div className="border rounded-lg border-slate-300 m-1 flex flex-col p-1  bg-white desktop:drop-shadow duration-300 hover:p-0 ">
         <iframe
           title="Polo"
@@ -68,7 +70,6 @@ export default function DShirts() {
           </Link>
         </div>
       </div>
-
       <div className="border rounded-lg border-slate-300 m-1 flex flex-col p-1  bg-white desktop:drop-shadow duration-300 hover:p-0 ">
         <iframe
           title="T-Shirt"
